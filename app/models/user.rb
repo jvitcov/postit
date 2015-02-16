a7c58db2f46f6a3fc8a3f8ab27673a810b6522d6
Justin Vitcov <vitcovja@gmail.com>
2015-02-16 12:20:32 -0800

    more minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ActiveRecord::Base
 	has_secure_password validations: false
 	validates :username, presence: true, uniqueness: true
 	validates :password, presence: true, on: :create, length: {minimum: 5}
-	validates :phone, numericality: {only_interger: true}
 
 	after_validation :generate_slug!
 
